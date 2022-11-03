ed1ba94643721f50d07d5656ecaf246f0dc33674
arfan36 <arfanur36@gmail.com>
2022-11-03 18:56:59 +0600

    67-3 Load services data and create checkout page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -12,18 +12,26 @@ app.use(express.json());
 
 // mongoDb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterarfan36.opuzllc.mongodb.net/?retryWrites=true&w=majority`;
-console.log('uri :>> ', uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
 
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const result = await serviceCollection.insertOne(user);
-            res.send(result);
+        // Read (R)
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // Read (R) specific item
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         });
     }
 
