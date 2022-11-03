b63e47584947c8ce66c4c734aab47b2f056f4d08
arfan36 <arfanur36@gmail.com>
2022-11-03 15:46:17 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +10,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterarfan36.opuzllc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+
 app.get('/', (req, res) => {
-    res.send('server running');
+    res.send('Hello from node mongo crud server');
 });
 
 app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+    console.log(`Listening to port ${port}`);
 });
