d6ab5133422a8652c697c8ffe032557a012a7cd7
arfan36 <arfanur36@gmail.com>
2022-11-07 14:32:30 +0600

    69-7 (Recap) JWT token verify and Handle 401, 403 to log out user

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ require('dotenv').config();
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        res.status(401).send({ message: 'unauthorized access' });
+        return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            res.status(401).send({ message: 'unauthorized access' });
+            return res.status(403).send({ message: 'unauthorized access' });
         }
         req.decoded = decoded;
         next();
@@ -39,7 +39,7 @@ async function run() {
         // Create (C) : token
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5' });
             res.send({ token });
         });
 
@@ -63,6 +63,11 @@ async function run() {
 
         // Read (R)
         app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('decoded, inside orders api :>> ', decoded);
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' });
+            }
 
             let query = {};
             if (req.query.email) {
